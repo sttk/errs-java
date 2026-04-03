ba48f2a3ac05eabc98770707cc74f9374667acc4
sttk <sttk.xslet@gmail.com>
2026-04-04 01:59:09 +0900

    comment: updated copyright years in source file headers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Takayuki Sato. All Rights Reserved.
+ * Copyright (C) 2025-2026 Takayuki Sato. All Rights Reserved.
  *
  * This program is free software under MIT License.
  * See the file LICENSE in this distribution for more details.
